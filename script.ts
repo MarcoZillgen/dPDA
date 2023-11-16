8b15e627fadd38ecae8413cd0e9be8d11764b1c4
Käkäsh <marco.zillgen23@gmail.com>
2023-11-16 20:30:19 +0100

    w

--- script.ts
+++ script.ts
@@ -89,35 +89,37 @@ class PDA {
         connection.condition.string == currentString
     );
   }
+
+  test(testString: CellarString[]) {
+    const test = this.run(testString);
+    if (test) console.log("Test passed");
+    else console.log("Test failed");
+  }
 }
 
-function testRun() {
-  const q1q1 = [
-    new Connection("q1", "q1", new Condition("a", "a", ["a", "a"])),
-    new Connection("q1", "q1", new Condition("a", "b", ["a", "b"])),
-    new Connection("q1", "q1", new Condition("b", "a", ["b", "a"])),
-    new Connection("q1", "q1", new Condition("b", "b", ["b", "b"])),
-    new Connection("q1", "q1", new Condition("a", "Z", ["a", "Z"])),
-    new Connection("q1", "q1", new Condition("b", "Z", ["b", "Z"])),
-  ];
+const q1q1 = [
+  new Connection("q1", "q1", new Condition("a", "a", ["a", "a"])),
+  new Connection("q1", "q1", new Condition("a", "b", ["a", "b"])),
+  new Connection("q1", "q1", new Condition("b", "a", ["b", "a"])),
+  new Connection("q1", "q1", new Condition("b", "b", ["b", "b"])),
+  new Connection("q1", "q1", new Condition("a", "Z", ["a", "Z"])),
+  new Connection("q1", "q1", new Condition("b", "Z", ["b", "Z"])),
+];
 
-  const q1q2 = [
-    new Connection("q1", "q2", new Condition("c", "a", ["a"])),
-    new Connection("q1", "q2", new Condition("c", "b", ["b"])),
-  ];
+const q1q2 = [
+  new Connection("q1", "q2", new Condition("c", "a", ["a"])),
+  new Connection("q1", "q2", new Condition("c", "b", ["b"])),
+];
 
-  const q2q2 = [
-    new Connection("q2", "q2", new Condition("a", "a", [null])),
-    new Connection("q2", "q2", new Condition("b", "b", [null])),
-  ];
+const q2q2 = [
+  new Connection("q2", "q2", new Condition("a", "a", [null])),
+  new Connection("q2", "q2", new Condition("b", "b", [null])),
+];
 
-  const q2q3 = new Connection("q2", "q3", new Condition(null, "Z", [null]));
+const q2q3 = new Connection("q2", "q3", new Condition(null, "Z", [null]));
 
-  const pda = new PDA([...q1q1, ...q1q2, ...q2q2, q2q3], "q1", "q3");
+const pda = new PDA([...q1q1, ...q1q2, ...q2q2, q2q3], "q1", "q3");
 
-  const testString: CellarString[] = ["a", "b", "a", "c", "a", "b", "a"];
+const testString: CellarString[] = ["a", "b", "a", "c", "a", "b", "a"];
 
-  console.log(pda.run(testString));
-}
-
-testRun();
+pda.test(testString);
